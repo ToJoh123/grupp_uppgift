d2d74fc796bbb069d28e8237b3354620366fcc4a
TomJ <tom.johansson@yh.nackademin.se>
2023-03-02 09:21:45 +0100

    land
    Check

---
+++ land.js
@@ -0,0 +1,19 @@
+const country =[
+    {
+        name: "India",
+        capital: "New Delhi",
+        population: 1300000000,
+
+    },
+    {
+        name: "Pakistan",
+        capital: "Islamabad",
+        population: 200000000,
+    },
+    {
+        name: "sweden",
+        capital:"Stockholm",
+        population: 1e+6
+    }
+]
+console.log(country[2].population)
